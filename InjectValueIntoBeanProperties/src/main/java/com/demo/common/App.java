923206e4ceabab368bdc2fac8ea9a496d255fe99
Bhavesh Murug <bmurug666@gmail.com>
2018-03-29 11:52:22 +0530

    How to inject value into bean properties in Spring

---
+++ InjectValueIntoBeanProperties/src/main/java/com/demo/common/App.java
@@ -0,0 +1,28 @@
+package com.demo.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.demo.pojo.SchoolInfo;
+import com.demo.pojo.StudentInfo;
+import com.demo.pojo.TeacherInfo;
+
+/**
+ * @author Bhavesh
+ *
+ */
+public class App 
+
+
+{
+    public static void main( String[] args )
+    {
+       ApplicationContext context=new ClassPathXmlApplicationContext("SpringBean.xml");
+       StudentInfo studentInfo=(StudentInfo)context.getBean("StudentInfo");
+       studentInfo.getStudentInfo();
+       TeacherInfo teacherInfo=(TeacherInfo)context.getBean("TeacherInfo");
+       teacherInfo.getTeacherinfo();
+       SchoolInfo schoolInfoInfo=(SchoolInfo)context.getBean("SchoolInfo");
+       schoolInfoInfo.getStudentInfo();
+    }
+}
