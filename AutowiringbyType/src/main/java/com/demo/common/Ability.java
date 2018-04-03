aff2fd80fd511708af276907566aa0913d97f988
Bhavesh Murug <bmurug666@gmail.com>
2018-04-03 13:02:48 +0530

    Spring Autowiring by Type

---
+++ AutowiringbyType/src/main/java/com/demo/common/Ability.java
@@ -0,0 +1,21 @@
+package com.demo.common;
+
+public class Ability {
+	
+	private String skill;
+
+	public String getSkill() {
+		return skill;
+	}
+
+	public void setSkill(String skill) {
+		this.skill = skill;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Ability [Skill "+skill+"]";
+	}
+
+}
