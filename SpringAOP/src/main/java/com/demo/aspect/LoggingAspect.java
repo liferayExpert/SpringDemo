2ef3d09c2491751785359bcac522fdb08d01dd2a
Bhavesh Murug <bmurug666@gmail.com>
2018-04-09 14:53:43 +0530

    pointCut and Whliecut AOP example.

--- SpringAOP/src/main/java/com/demo/aspect/LoggingAspect.java
+++ SpringAOP/src/main/java/com/demo/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.demo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 import com.demo.model.Maths;
 import com.demo.model.Science;
@@ -9,10 +10,22 @@ import com.demo.model.Science;
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String getBookName())")
+	@Before("allgetters()")
 	public void loggingAdvicer()
 	{
 		System.out.println("Logging Advicer is running before target :");
 	}
-
+	
+	@Before("allgetters()")
+	public void loggingAdvicer1()
+	{
+		System.out.println("Logging1 Advicer is running before target :");
+	}
+	
+	@Pointcut("execution(public * get*())")
+     public void allgetters() {
+	}	
 }
+
+
+
